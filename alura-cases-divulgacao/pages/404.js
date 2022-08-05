01e5007898b4d58d4d8dde3218aabf9a8d335c46
Gabriel Cardelli <cardelli@impa.br>
2022-08-04 21:14:02 -0300

    Versão inicial do projeto alura-cases da formação next

---
+++ alura-cases-divulgacao/pages/404.js
@@ -0,0 +1,8 @@
+
+export default function NotFoundPage(){
+    return (
+        <div>
+            <h1>PAGINA NAO ENCONTRADA22</h1>
+        </div>
+    )
+}
---
+++ alura-cases-divulgacao/pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FAQPage(){
+    return (
+        <div>
+            <h1>Alura Cases - FAQ</h1>
+            <Link href="/">HOME</Link>
+        </div>
+    )
+}
