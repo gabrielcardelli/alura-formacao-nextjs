2e415f59025d0ee41ff7ec24237e22854a26b4e7
Gabriel Cardelli <gabrielcastilho.cardelli@gmail.com>
2022-08-14 20:26:43 -0300

    Atualizando projeto até a aula 02.05 -> Separando configurações em comum entre os módulos em um módulo de stup

---
+++ nextjs-arquitetura/projects/web-public/pages/_app.tsx
@@ -0,0 +1,38 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme,createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    color: ${({ theme }) => theme.colors.primary};
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#111',
+    secondary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
