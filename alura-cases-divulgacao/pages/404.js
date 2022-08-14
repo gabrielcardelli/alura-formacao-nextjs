977207246004e9c3d10ec3cfbdb82fee3a092cff
Gabriel Cardelli <gabrielcastilho.cardelli@gmail.com>
2022-08-14 19:34:18 -0300

    Seperando projetos em modulos do workspace de acordo com a aula 2 do módulo 2

--- alura-cases-divulgacao/pages/404.js
+++ alura-cases-divulgacao/pages/404.js
@@ -3,6 +3,7 @@ export default function NotFoundPage(){
     return (
         <div>
             <h1>PAGINA NAO ENCONTRADA22</h1>
+            <h2></h2>
         </div>
     )
 }
---
+++ nextjs-arquitetura/projects/web-public/pages/index.tsx
@@ -0,0 +1,10 @@
+import {sum} from "@alura/utils/math/sum"
+
+export default function HomeScreen() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>Importando modulo local: @alura/sum (sum2,2) = {sum(2,2)}</p>
+    </div>
+  );
+}
