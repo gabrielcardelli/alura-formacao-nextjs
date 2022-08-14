bffc03b46eb2078a50fddf027698c16c108f1b3d
Gabriel Cardelli <gabrielcastilho.cardelli@gmail.com>
2022-08-14 19:58:38 -0300

    Atualização do projeto até a aula 02.03 -> Implementada transpilação de arquivos typescript para javascript dentro de workspaces

--- nextjs-arquitetura/projects/web-public/pages/index.tsx
+++ nextjs-arquitetura/projects/web-public/pages/index.tsx
@@ -1,9 +1,10 @@
 import {sum} from "@alura/utils/math/sum"
+import {Text} from "@alura/design-system/components/Text"
 
 export default function HomeScreen() {
   return (
     <div>
-        <h1>Home</h1>
+        <Text tag="h1">Home</Text>
         <p>Importando modulo local: @alura/sum (sum2,2) = {sum(2,2)}</p>
     </div>
   );
